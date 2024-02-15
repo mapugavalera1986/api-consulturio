5335c7a53c9c9896371fe66442215fc8f8a45921
mapugavalera <mapugavalera@gmail.com>
2024-02-15 14:26:29 -0500

    Algunos preparativos

--- src/main/java/pe/pruebaeita/servicios/IEspecialistaService.java
+++ src/main/java/pe/pruebaeita/servicios/IEspecialistaService.java
@@ -5,11 +5,10 @@ import java.util.Optional;
 
 import pe.pruebaeita.transferencias.EspecialistaDto;
 
-
 public interface IEspecialistaService {
 	List<EspecialistaDto> listarTodos();
-	/*Optional<TerapeutaDto> buscar(int id);
-	TerapeutaDto agregar(TerapeutaDto c_nuevo);
-	TerapeutaDto modificar(int id, TerapeutaDto c_cambiar);
-	void eliminar(int id);*/
+	Optional<EspecialistaDto> buscar(int id);
+	EspecialistaDto agregar(EspecialistaDto espc_nuevo);
+	EspecialistaDto modificar(int id, EspecialistaDto espc_modificar);
+	void eliminar(int id);
 }
